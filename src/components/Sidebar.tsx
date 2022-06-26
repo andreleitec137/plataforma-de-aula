77ce6bd7da3a60893a75fc7aa6eb72ec4a924cbb
aleitec137 <aleite@marinformatica.com.br>
2022-06-26 11:51:18 -0300

    Aula 5

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,30 +1,9 @@
-import { gql, useQuery } from "@apollo/client";
+import { useGetLessonsQuery } from "../graphql/generated";
 import { Lesson } from "./Lesson";
 
-const GET_LESSONS_QUERY = gql `
-query  {
-  lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
-    id
-    slug
-    title
-    lessonType
-    availableAt
-  }
-}
-`
-
-interface GetLessonsQueryReponse {
-    lessons: {
-        id: string
-        slug: string
-        title: string
-        lessonType: 'live' | 'class'
-        availableAt: string
-    }[]
-}
 
 export function Sidebar(){
-    const {data} = useQuery<GetLessonsQueryReponse>(GET_LESSONS_QUERY)
+    const {data} = useGetLessonsQuery()
 
     return(
         <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
